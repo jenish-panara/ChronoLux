9d681fceb1e245ca01c51f45052f6a27787d727f
jenishpanara <159444898+jenish-panara@users.noreply.github.com>
2026-06-25 17:48:00 +0530

    Add domain in next config file

--- ChronoLux/frontend/next.config.mjs
+++ ChronoLux/frontend/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
       {
         source: '/api/:path*',
         destination:
-          'http://localhost:5000/api/:path*',
+          'https://chronolux-ytwp.onrender.com/api/:path*',
       },
     ];
   },
