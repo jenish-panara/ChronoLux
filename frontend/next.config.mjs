f3e20b2595ecb7d694ba94025f463637352698ff
jenishpanara <159444898+jenish-panara@users.noreply.github.com>
2026-07-17 11:12:07 +0530

    Update API destination URL in next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
       {
         source: '/api/:path*',
         destination:
-          'https://chronolux-ytwp.onrender.com/api/:path*',
+          'https://chronolux-bgia.onrender.com//api/:path*',
       },
     ];
   },
